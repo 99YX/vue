70bb04f93358adecd7f67ce34e216399c95a4ba7
yanixn <2896399920@qq.com>
2022-12-24 01:46:54 +0800

    路由

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,7 +26,7 @@
     <!--设置下拉效果-->
     <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
 
-    <el-dropdown-menu slot="dropdown">
+    <el-dropdown-menu >
       <el-dropdown-item>个人信息</el-dropdown-item>
       <el-dropdown-item>退出</el-dropdown-item>
       <el-dropdown-item>删除</el-dropdown-item>
